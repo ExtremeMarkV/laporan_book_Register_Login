a87c040f0c1a9888898521ef35054caa9d24e6d0
ExtremeMarkV <aryadanapriyatama00@gmail.com>
2023-12-27 21:58:05 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# laporan_book
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/laporan_book/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.laporan_book
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
